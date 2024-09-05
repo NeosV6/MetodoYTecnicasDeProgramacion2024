588c18799044a96de3c067f4aee4f0f300963440
edwcand <167575932+AlexCandia@users.noreply.github.com>
2024-09-05 11:44:39 -0400

    Modificacion meodo

---
+++ ProyectoJose/src/main/java/com/mycompany/proyectojose/ProyectoJose.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectojose;
+
+/**
+ *
+ * @author developerweb
+ */
+public class ProyectoJose {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
