305296509f9666de2a989bd36734f1c2e424b0a9
edwcand <167575932+AlexCandia@users.noreply.github.com>
2024-09-05 10:32:35 -0400

    Creacion clase Usuario

---
+++ MetodosYTecnicasDeProgramacion22024/src/main/java/com/mycompany/metodosytecnicasdeprogramacion22024/Usuario.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.metodosytecnicasdeprogramacion22024;
+
+/**
+ *
+ * @author developerweb
+ */
+public class Usuario {
+    public int nombre;
+    public int nacceso;
+    public Usuario(int nombre,int nacceso){
+        this.nombre=nombre;
+        this.nacceso=nacceso;
+    }
+}
