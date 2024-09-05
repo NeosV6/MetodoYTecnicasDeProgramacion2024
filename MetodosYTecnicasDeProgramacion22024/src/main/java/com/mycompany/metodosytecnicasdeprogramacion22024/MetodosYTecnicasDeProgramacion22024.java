1055319a3ca135b647fcb9842b8a38e21f2cf31b
edwcand <167575932+AlexCandia@users.noreply.github.com>
2024-09-05 10:26:36 -0400

    Primer Archivo

---
+++ MetodosYTecnicasDeProgramacion22024/src/main/java/com/mycompany/metodosytecnicasdeprogramacion22024/MetodosYTecnicasDeProgramacion22024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.metodosytecnicasdeprogramacion22024;
+
+/**
+ *
+ * @author developerweb
+ */
+public class MetodosYTecnicasDeProgramacion22024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
