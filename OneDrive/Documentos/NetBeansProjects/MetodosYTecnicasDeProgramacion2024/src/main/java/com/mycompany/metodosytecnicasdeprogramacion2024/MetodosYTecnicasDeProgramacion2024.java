694ddb81d4e2cc559e9676ef8fbb9a087d1e9404
Candy <168691418+Amiddala@users.noreply.github.com>
2024-09-05 11:23:03 -0400

    Primer codigo

---
+++ OneDrive/Documentos/NetBeansProjects/MetodosYTecnicasDeProgramacion2024/src/main/java/com/mycompany/metodosytecnicasdeprogramacion2024/MetodosYTecnicasDeProgramacion2024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.metodosytecnicasdeprogramacion2024;
+
+/**
+ *
+ * @author Camila
+ */
+public class MetodosYTecnicasDeProgramacion2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World holi!");
+    }
+}
