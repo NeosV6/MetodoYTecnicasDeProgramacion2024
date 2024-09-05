ae59044da2b8e4c10faf62d874409c34dafc7b6c
NeosV6 <velahinojosajosealfredo@gmail.com>
2024-09-05 10:58:51 -0400

    aver

---
+++ MetodosYTecnicasDeProgramacion22024/src/main/java/com/mycompany/metodosytecnicasdeprogramacion22024/enviadoEnBranchMaster.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.metodosytecnicasdeprogramacion22024;
+
+/**
+ *
+ * @author Usuario
+ */
+public class enviadoEnBranchMaster {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
