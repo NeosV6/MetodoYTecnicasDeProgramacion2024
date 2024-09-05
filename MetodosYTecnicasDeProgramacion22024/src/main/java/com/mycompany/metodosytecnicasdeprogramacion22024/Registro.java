9c1d6e799a63110f4c3de0d7211c8bbe9e275795
NeosV6 <velahinojosajosealfredo@gmail.com>
2024-09-05 10:48:57 -0400

    Clase registro

---
+++ MetodosYTecnicasDeProgramacion22024/src/main/java/com/mycompany/metodosytecnicasdeprogramacion22024/Registro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.metodosytecnicasdeprogramacion22024;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Registro {
+
+    public String nombre;
+    public int contraseña;
+    public String Rango;
+    public Registro(String nombre,int contraseña, String Rango){
+        this.nombre=nombre;
+        this.contraseña=contraseña;
+        this.Rango=Rango;    }
+}
