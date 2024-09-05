655346a4ed3fd30d9edbba399655ab118cc8ffe4
Candy <168691418+Amiddala@users.noreply.github.com>
2024-09-05 10:42:39 -0400

    Clase Inicio

---
+++ OneDrive/Documentos/NetBeansProjects/MetodosYTecnicasDeProgramacion22024/src/main/java/com/mycompany/metodosytecnicasdeprogramacion22024/Inicio.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Camila
+ */
+public class Inicio extends Usuario {
+    private Usuario usr;
+    public Inicio(){}
+    
+}
